850d0439e113cfd0c849336c8ece6f220d5ff815
Amoura Mohcene <102263342+MohceneAmoura@users.noreply.github.com>
2025-03-07 20:04:41 +0100

    Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/send-email`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
